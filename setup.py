0dad7788a4987cfa1078eea7d9123210d68d1e78
Fabien Reboia <srounet@gmail.com>
2010-09-24 00:05:19 +0200

    Added license to python files

--- setup.py
+++ setup.py
@@ -1,3 +1,14 @@
+# "THE POSTCARD LICENSE" (Revision 0):
+# Fabien Reboia wrote this file. As long as you retain this notice you
+# can do whatever you want with this stuff. If you like the work you can
+# send me a post card in return at this address :
+
+# Fabien Reboia
+# France,
+# 110 rue jean le galleu
+# 94200 Ivry sur seine
+
+
 from distutils.core import setup
 
 
