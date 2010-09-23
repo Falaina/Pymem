d30133298754236fd34aac45b8fcfea35f9c1d78
Fabien Reboia <srounet@gmail.com>
2010-09-23 23:19:12 +0200

    Added pymem class and modified LICENSE

--- pymem.py
+++ pymem.py
@@ -1,6 +1,3 @@
-Copyright (c) 2010 Pymem
-
-----------------------------------------------------------------------------
 # "THE POSTCARD LICENSE" (Revision 0):
 # Fabien Reboia wrote this file. As long as you retain this notice you
 # can do whatever you want with this stuff. If you like the work you can
@@ -10,4 +7,11 @@ Copyright (c) 2010 Pymem
 # France,
 # 110 rue jean le galleu
 # 94200 Ivry sur seine
-----------------------------------------------------------------------------
+
+
+class Pymem(object):
+    def __init__(self):
+        """Provides class instance methods for general process and memory
+manipulations.
+        """
+        pass
