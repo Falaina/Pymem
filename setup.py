18fcecb13fe23ce48be29af5e0eecd88589741d4
Fabien Reboia <srounet@gmail.com>
2010-09-24 00:04:07 +0200

    Added setup.py, moved pymem class to pymem and added unittest folder

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+
+setup(name='Pymem',
+      version='0.1',
+      description='pymem: python memory access made easy',
+      author='Fabien Reboia',
+      author_email='srounet@gmail.com',
+      maintainer='Fabien Reboia',
+      maintainer_email='srounet@gmail.com',
+      url=' http://www.pymem.org/',
+      packages=['pymem'],
+      long_description="A python library for windows, providing the needed \
+functions to start working on your own with memory editing",
+      license="postcard license",
+      platforms=["windows"]
+)
---
+++ unit/test.py
@@ -0,0 +1,16 @@
+import unittest
+import pymem
+
+
+class TestPymemClass(unittest.TestCase):
+    """TestPymemClass"""
+
+    def setUp(self):
+        pass
+
+    def test_create(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
