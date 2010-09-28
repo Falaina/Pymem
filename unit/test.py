d36ea5930e5d19886f207d88c914c269248a65bc
Fabien Reboia <srounet@gmail.com>
2010-09-28 22:03:32 +0200

    modified unittest and changed unittest to pep8.

--- unit/test.py
+++ unit/test.py
@@ -13,21 +13,20 @@ class TestPymemClass(unittest.TestCase):
 
     def test_open_from_id(self):
         self.assertTrue(self.pymem.open_process(2124))
-        
-    def test_read(self):
-        PLAYER_BASE = 0xCD87A8
-        PLAYER_BASE_OFFSET1 = 0x34
-        PLAYER_BASE_OFFSET2 = 0x24
-        self.pymem.open_process_from_name('Wow')
-        pointer  = self.pymem.read_offset([0xCD87A8 - 0x8, 0x34, 0x24], 'uint')
-        print self.pymem.read_offset(pointer + 0x798, 'float')
 
-	def test_read_string(self):
-		print 'testing string'
-		self.pymem.open_process_from_name('Wow')
-        print self.pymem.read_offset(0xC79D18 - 0x8, 'string')
-        
-        
+    def test_read(self):
+        self.pymem.open_process_from_name('Wow')
+        pointer = self.pymem.read_offset([0xCD87A8 - 0x8, 0x34, 0x24],\
+        'uint')
+        self.assertNotEqual(self.pymem.read_offset(pointer + 0x798,\
+        'float'), 0x0)
+
+    def test_read_string(self):
+        self.pymem.open_process_from_name('Wow')
+        self.assertNotEqual(self.pymem.read_offset(0xC79D18 - 0x8, 'string'),\
+        '')
+
 
 if __name__ == '__main__':
-    unittest.main()
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestPymemClass)
+    testResult = unittest.TextTestRunner(verbosity=2).run(suite)
